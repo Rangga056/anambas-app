26a4ed17daa5ca4ffb9ab7646812a86bff5c5a5d
Yoga Aditya Nugraha <yoga19201@gmail.com>
2024-09-04 00:59:30 -0700

    Make Migration & API Login Register

---
+++ database/migrations/2024_09_04_075214_create_disctricts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disctricts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userID');
+            $table->unsignedBigInteger('highlightID');
+            $table->foreign('userID')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('highlightID')->references('id')->on('highlights')->cascadeOnDelete();
+            $table->string('heroIMG')->nullable(false);
+            $table->string('disctrictName')->nullable(false);
+            $table->string('disctrictDescription')->nullable(false);
+            $table->string('location')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disctricts');
+    }
+};
