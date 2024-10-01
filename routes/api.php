94b0e051ad86e55fbff2d4241d098a2fc29e561c
Afzalkhm <afzalkhm1203@gmail.com>
2024-10-01 21:24:33 +0700

    instal Swagger for doc API and make API not Public

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/',function(){
+    return response()->json([
+        'status'=>false,
+        'message'=>'You are not allowed to get this acces'
+    ],401);
+})->name('login');
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/datauser', [AuthController::class, 'datauser']);
-Route::post('/admin/{id}/delete', [AuthController::class, 'removeuser']);
-Route::post('/admin/{id}/verify', [AuthController::class, 'verifyuser']);
+Route::get('/datauser', [AuthController::class, 'datauser'])->middleware('auth:sanctum');
+Route::post('deleteuser/{id}', [AuthController::class, 'removeuser']);
+Route::post('verifyuser/{id}', [AuthController::class, 'verifyuser']);
 Route::get('/filteruser', [AuthController::class, 'filterdateuser']);
 Route::post('/searchuser', [AuthController::class, 'searchuser']);
