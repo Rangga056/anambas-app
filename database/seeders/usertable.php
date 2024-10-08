60f68f89fb991319203219f418c3ab87be76cea6
Afzalkhm <afzalkhm1203@gmail.com>
2024-10-08 23:09:38 +0700

    add logic districtdashboard

---
+++ database/seeders/usertable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class usertable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        factory(\App\User::class, 10)->create([
+            'name'	=> str_random(20),
+            'email'	=> str_random(10) . '@anambas.gmail.com',
+            'password'	=> Hash::make('12345678')
+    ]);
+    }
+}
