e499451d85e3b8f035e4447f4381d8891131c302
Rangga056 <muhammadrangga056@gmail.com>
2024-10-24 09:31:43 +0700

    trigger vercel redeploy

--- app/dashboard/site-admin/page.jsx
+++ app/dashboard/site-admin/page.jsx
@@ -1,11 +1,15 @@
 "use client";
 
 import withAuth from "@/app/hoc/withAuth";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const SiteAdminPage = () => {
   return (
     <div>
+      <div>
+        <Input type="text" name="search" />
+      </div>
       <div>This is site admin Page</div>
     </div>
   );
