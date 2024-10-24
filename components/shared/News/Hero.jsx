b54d4e4b3fa1c41e3b90ea572f67cef2b8895f16
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-10-24 09:18:25 +0700

    fix(news):  image name typo in import

--- components/shared/News/Hero.jsx
+++ components/shared/News/Hero.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-
-import newsHeroImage from '@/public/assets/images/koala.jpeg'
+import newsHeroImage from "@/public/assets/images/koala.jpg";
 
 //TODO: Update ketika sudah ada data
 
@@ -13,28 +12,27 @@ const Hero = () => {
         {/* Left section */}
         {/* Update this when the image is ready */}
         <div className="w-[850px] h-[321px] rounded-xl overflow-hidden relative">
-        <Image
-          src={newsHeroImage}
-          layout="fill" 
-          objectFit="cover"
-          alt="koala"
-        />
-      </div>
+          <Image
+            src={newsHeroImage}
+            layout="fill"
+            objectFit="cover"
+            alt="koala"
+          />
+        </div>
 
         {/* Right section */}
         <div className="w-full flex flex-col items-start gap-y-4">
-        <p className="body text-black">Penerbit &#8226; dd/mm/yy</p>
-        <h1 className="header-2 uppercase" >lorem ipsum dolor sit amet</h1>
+          <p className="body text-black">Penerbit &#8226; dd/mm/yy</p>
+          <h1 className="header-2 uppercase">lorem ipsum dolor sit amet</h1>
           <p className="paragraph-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-            <br />
-           {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+            <br />{" "}
           </p>
         </div>
       </div>
