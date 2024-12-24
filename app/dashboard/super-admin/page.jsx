0f52507fab643ad8502efc196f8edb39ac05c22b
Rangga056 <muhammadrangga056@gmail.com>
2024-12-24 20:42:31 +0700

    feat(super-admin): add fetch function for getting all user data

--- app/dashboard/super-admin/page.jsx
+++ app/dashboard/super-admin/page.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+"use client";
+
+import { useUsersStore } from "@/app/stores/userStore";
+import { fetchUsers } from "@/lib/actions/site-admin/activity.actions";
+import React, { useEffect, useState } from "react";
 
 const SuperAdminPage = () => {
+  const [data, setData] = useState([]);
+  // Define the getData function
+  const getData = async () => {
+    try {
+      const fetchedData = await fetchUsers();
+      console.log(fetchedData);
+      console.log(data);
+      setData(fetchedData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Fetch data on initial render
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <div>This is Super admin Page</div>
+      <h1>This is Super Admin Page</h1>
+      <ul>
+        {data?.length ? (
+          data.map((item, index) => (
+            <li key={index}>
+              <strong>{item.username}</strong> - {item.email} - {item.role} -{" "}
+              {item.date}
+            </li>
+          ))
+        ) : (
+          <p>Loading user data...</p>
+        )}
+      </ul>
     </div>
   );
 };
--- app/stores/userStore.js
+++ app/stores/userStore.js
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
 export const useUserActivityStore = create((set) => ({
-  data: null,
+  data: [],
+  setData: (data) => set({ data }),
+}));
+
+export const useUsersStore = create((set) => ({
+  data: [],
   setData: (data) => set({ data }),
 }));
